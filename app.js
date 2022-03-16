8c1979224b27df33fde53bde5b4a953d19f22b0b
osmanforhad <osmanforhad93@gmail.com>
2022-03-16 13:49:20 +0600

    setup route for app

--- app.js
+++ app.js
@@ -25,6 +25,75 @@ app.get("/", (request, response) => {
   response.render("index.pug");
 });
 
+app.post("/paystack/pay", (request, response) => {
+  const form = _.pick(request.body, ["amount", "email", "full_name"]);
+  form.metadata = {
+    full_name: form.full_name,
+  };
+  form.amount *= 100;
+  initializePayment(form, (error, body) => {
+    if (error) {
+      //handle errors
+      console.log(error);
+      return response.redirect("/error");
+      return;
+    }
+    response = JSON.parse(body);
+    response.redirect(response.addTrailers.authorization_url);
+  });
+});
+
+app.get("/paystack/callback", (request, response) => {
+  const ref = request.query.reference;
+  verifyPayment(ref, (error, body) => {
+    if (error) {
+      //handle errors appropriately
+      console.log(error);
+      return response.redirect("/error");
+    }
+    response = JSON.parse(body);
+
+    const data = _.at(response.data, [
+      "reference",
+      "amount",
+      "customer.email",
+      "metadata.full_name",
+    ]);
+    [reference, amount, email, full_name] = data;
+    newDonor = { reference, amount, email, full_name };
+    const donor = new Donor(newDonor);
+    donor
+      .save()
+      .then((donor) => {
+        if (!donor) {
+          return response.redirect("/error");
+        }
+        response.redirect("/receipt/" + donor._id);
+      })
+      .catch((e) => {
+        response.redirect("/error");
+      });
+  });
+});
+
+app.get("/receipt/:id", (request, response) => {
+  const id = request.params.id;
+  Donor.findById(id)
+    .then((donor) => {
+      if (!donor) {
+        response.redirect("/error");
+      }
+      response.render("success.pug", { donor });
+    })
+    .catch((e) => {
+      response.redirect("/error");
+    });
+});
+
+app.get("/error", (request, response) => {
+  response.render("error.pug");
+});
+
 app.listen(port, () => {
   console.log(`App is Running on port ${port}`);
 });
